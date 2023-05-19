fdb6f98ebc08ae9482d83304f0f712bbd6f0b422
Luciano Abreu <lzabreu@mac.com>
2023-05-19 17:25:34 -0300

    Fifth Commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  distDir: 'dist',
+  // distDir: 'dist',
   images: {
     unoptimized: true,
     domains: ['ergast.com'],
